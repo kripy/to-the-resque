300063f686d2a4da7ba3471e343092cdebe1c1db
Arturo Escartin <arturo@kripy.com>
2013-07-27 16:03:50 +1000

    Kill resque workers.

--- unicorn.rb
+++ unicorn.rb
@@ -2,10 +2,10 @@ worker_processes 4 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
 preload_app true   # avoid regeneration of jekyll site for each fork
 
-@resque_pid = nil
-@resque_pid2 = nil
+#@resque_pid = nil
+#@resque_pid2 = nil
 
-before_fork do |server, worker|
-  @resque_pid ||= spawn("bundle exec rake resque:work TERM_CHILD=1")
-  @resque_pid2 ||= spawn("bundle exec rake resque:work TERM_CHILD=1")
-end
+#before_fork do |server, worker|
+  #@resque_pid ||= spawn("bundle exec rake resque:work TERM_CHILD=1")
+  #@resque_pid2 ||= spawn("bundle exec rake resque:work TERM_CHILD=1")
+#end
