9b352e92e61ad3f1ce9970ac23349de828373c52
Arturo Escartin <arturo@kripy.com>
2013-07-28 21:12:22 +1000

    Tweaked the worker call.

--- unicorn.rb
+++ unicorn.rb
@@ -6,6 +6,6 @@ preload_app true
 @resque_pid2 = nil
 
 before_fork do |server, worker|
-  @resque_pid ||= spawn("bundle exec rake resque:work TERM_CHILD=1")
-  @resque_pid2 ||= spawn("bundle exec rake resque:work TERM_CHILD=1")
+  @resque_pid ||= spawn("env TERM_CHILD=1 bundle exec rake resque:work")
+  @resque_pid2 ||= spawn("env TERM_CHILD=1 bundle exec rake resque:work")
 end
